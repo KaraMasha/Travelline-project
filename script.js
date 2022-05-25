1743e40ff60d5c300d4e7303b6be257011ff2cb9
KaraMasha <karamasha94@gmail.com>
2022-05-25 16:06:48 +0300

    last updates

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function() {
     const burgerButton = document.getElementById('burger-button');
-
+    console.log(burgerButton);
     burgerButton.addEventListener('click', function(event) {
         event.preventDefault();
         burgerButton.classList.toggle('opened');
